60d03e288924eb06c960516857aa212f2a8c8cb9
gkaragoz <gkaragoz96@gmail.com>
2018-02-01 20:34:43 +0300

    Add Utility.cs

---
+++ Sailor-io/Assets/Scripts/Utility.cs
@@ -0,0 +1,12 @@
+﻿
+public static class Utility {
+
+    public static float Map(float value, float low1, float high1, float low2, float high2) {
+        if (high1 - low1 == 0f) {
+            return value;
+        }
+
+        return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+    }
+
+}
