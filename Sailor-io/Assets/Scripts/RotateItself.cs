156934a2db598c4935d19a9e3b317333d0ffa21a
gkaragoz <gkaragoz96@gmail.com>
2018-02-02 20:06:51 +0300

    Add RotateItself.cs script

---
+++ Sailor-io/Assets/Scripts/RotateItself.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateItself : MonoBehaviour {
+
+    [Range(5f, 30f)]
+    public float speed = 5f;
+    public bool clockwise = false;
+
+    private void Awake() {
+        if (Random.Range(0, 1f) <= 0.5f)
+            clockwise = true;
+
+        speed = Random.Range(5f, 30f);
+    }
+
+    private void Update() {
+        transform.Rotate(new Vector3(speed, speed, speed) * Time.deltaTime);
+    }
+
+}
