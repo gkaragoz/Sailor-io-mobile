c81ce3f36153cb9c322e9e3b9ebf562d4f93ea11
gkaragoz <gkaragoz96@gmail.com>
2018-02-03 20:13:39 +0300

    Create GameManager script and Change the RandomSupplyGenerator to SupplyGenerator

---
+++ Sailor-io/Assets/Scripts/Client/SupplyGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupplyGenerator : MonoBehaviour {
+
+    private Transform parentObject;
+
+    private void Awake() {
+        if (GameObject.Find("_GeneratedSupplies") == null) {
+            parentObject = new GameObject("_GeneratedSupplies").transform;
+        } else {
+            parentObject = GameObject.Find("_GeneratedSupplies").transform;
+        }
+    }
+
+    private GameObject InstantiateNewSupply(GameObject prefab, Vector3 position) {
+        if (prefab == null) {
+            Debug.LogError("!Assign a SUPPLY!");
+            return null;
+        }
+
+        GameObject supply = Instantiate(prefab, position, Quaternion.identity, parentObject);
+        return supply;
+    }
+
+}
---
+++ Sailor-io/Assets/Scripts/Client/WorldManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour {
+
+    public static WorldManager instance;
+
+    [Header("Initialize")]
+    public Transform mapObject;
+
+    [Header("Good to know")]
+    public Vector3 MAP_CENTER;
+    public float MAP_LENGTH;
+    public float MAP_WIDTH;
+    public float MAP_HEIGHT;
+
+    private SupplyGenerator supplyGenerator; 
+
+    private void Awake() {
+        if (instance == null)
+            instance = this;
+
+        supplyGenerator = GetComponent<SupplyGenerator>();
+    }
+
+    public void BuildWorld() {
+
+    }
+
+    public Vector3 GetMapCenter() {
+        if (mapObject == null) {
+            Debug.LogError("Assign a map!");
+            return Vector3.zero;
+        }
+        MAP_CENTER = new Vector3(mapObject.position.x, mapObject.position.y, mapObject.position.z);
+        return MAP_CENTER;
+    }
+
+    public float GetMapHeight() {
+        if (mapObject == null) {
+            Debug.LogError("Assingn a map!");
+            return 100f;
+        }
+        float centerOffset = GetMapCenter().y;
+        MAP_HEIGHT = centerOffset + (mapObject.GetComponent<MeshRenderer>().bounds.size.y * 0.5f);
+        return MAP_HEIGHT;
+    }
+
+    public float GetMapLength() {
+        if (mapObject == null) {
+            Debug.LogError("Assign a map!");
+            return 100f;
+        }
+        float centerOffset = GetMapCenter().x;
+        MAP_LENGTH = centerOffset + (mapObject.GetComponent<MeshRenderer>().bounds.size.x * 0.5f);
+        return MAP_LENGTH;
+    }
+
+    public float GetMapWidth() {
+        if (mapObject == null) {
+            Debug.LogError("Assign a map!");
+            return 100f;
+        }
+        float centerOffset = GetMapCenter().z;
+        MAP_WIDTH = centerOffset + (mapObject.GetComponent<MeshRenderer>().bounds.size.z * 0.5f);
+        return MAP_WIDTH;
+    }
+
+}
