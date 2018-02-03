dc4b0edf3637b17ae595256ea94775c5ff68a760
can <gurcaglarogulcan@gmail.com>
2018-02-03 17:19:17 +0300

    Add Supply data. World info model.

---
+++ Sailor-io/Assets/Scripts/Network/ApiModels/SupplyCrate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Network.ApiModels
+{
+	[Serializable]
+	public class SupplyCrate
+	{
+		public double pos_x { get; set; }
+		public double pos_z { get; set; }
+		public double pos_y { get; set; }
+		public string supplyId { get; set; }
+		public int supplyIncome { get; set; }
+		public string supplyName { get; set; }
+	}
+}
