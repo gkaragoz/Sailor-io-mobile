4524dfa095df72f2e2bf4fe5efab74eb423bb365
gkaragoz <gkaragoz96@gmail.com>
2018-02-24 15:55:41 +0300

    Clean up the UIManager.cs script and take some notes into AuthManager.cs

--- Sailor-io/Assets/Scripts/Client/UIManager.cs
+++ Sailor-io/Assets/Scripts/Client/UIManager.cs
@@ -8,69 +8,14 @@ public class UIManager : MonoBehaviour
 {
 	public static UIManager instance;
 
-	#region LoginScreen
-
-	public string GameServerUrl;
-	private Button btnEnter;
-	private InputField inputNickname;
-	private InputField inputPassword;
-	private string LogonUserEmail;
-	private string LogonUserId;
-	private string GameAccessToken;
-	#endregion
-	void Awake()
-	{
+    private void Awake() {
 		if (instance == null)
 			instance = this;
 
 		DontDestroyOnLoad(instance);
-		GetUIReferences();
-		AssignBtnDelegates();
 	}
 
-	private void AssignBtnDelegates()
-	{
-		btnEnter.onClick.AddListener(delegate
-		{
-			OnLogin();
-		});
-	}
-
-	private void GetUIReferences()
-	{
-		btnEnter = GameObject.Find("btnEnter").GetComponent<Button>();
-		inputNickname = GameObject.Find("inputNickname").GetComponent<InputField>();
-		inputPassword = GameObject.Find("inputPassword").GetComponent<InputField>();
-	}
-
-	private void OnLogin()
-	{
-		string email = inputNickname.text;
-		string password = inputPassword.text;
-
-		if (string.IsNullOrEmpty(email) && string.IsNullOrEmpty(password))
-			return;
-
-		#region LoginRequest
-		var authResult = AuthManager.instance.doPostLogin(email, password);
-		if (authResult != null)
-		{
-			var userField = authResult.GetField("user");
-			var tokenField = authResult.GetField("token");
-
-			LogonUserEmail = userField.GetField("email").ToString();
-			LogonUserId = userField.GetField("id").ToString();
-			GameAccessToken = tokenField.GetField("accessToken").ToString();
-
-			Debug.Log("Successfully logged in ! >> " + LogonUserId);
-			Debug.Log("Welcome ! >> " + LogonUserEmail);
-			ChangeScene("Sandbox");
-		}
-		#endregion
-	}
-
-	public void ChangeScene(string sceneName)
-	{
+	public void ChangeScene(string sceneName) {
 		SceneManager.LoadScene(sceneName);
 	}
 }
