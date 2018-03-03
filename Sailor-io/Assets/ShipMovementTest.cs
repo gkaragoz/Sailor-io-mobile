5240aa498119705de9903a48a9a0f3fa75a0c4c7
huseyinyasar <huseyinyassar@gmail.com>
2018-03-04 02:45:30 +0300

    Character switching between free running and captain mode

--- Sailor-io/Assets/ShipMovementTest.cs
+++ Sailor-io/Assets/ShipMovementTest.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementTest : MonoBehaviour {
+public class ShipMovementTest : MonoBehaviour {
 	public float shipSpeed;
 	public float shipRotationSpeed;
 	public float wheelRotationSpeed;
@@ -13,18 +13,25 @@ public class MovementTest : MonoBehaviour {
 	private float shipRotation = 0;
 	private float wheelRotation = 0;
 
+	//ship animations
 	private int turnRightHash = Animator.StringToHash("Turn Right");
 	private int turnLeftHash = Animator.StringToHash("Turn Left");
 	private int idleHash = Animator.StringToHash("Idle");
 
 	public GameObject captain;
+	public bool captainAtWheel;
+
+	//captain animations
 	private int steerRightHash = Animator.StringToHash("Steer Right");
 	private int steerLeftHash = Animator.StringToHash("Steer Left");
 	private int idleCaptainHash = Animator.StringToHash("Steer Idle");
 
 	private float animationSpeed;
 
+	public Transform captainPosition;
+
 	void Start () {
+		captainAtWheel = false;
 		animationSpeed = captain.GetComponent<Animator> ().speed;
 	}
 
@@ -32,85 +39,97 @@ public class MovementTest : MonoBehaviour {
 		//move ship forward
 		transform.Translate (Vector3.forward * shipSpeed * Time.deltaTime);
 
-		RotateWheel ();
+		if (captainAtWheel)
+			RotateWheel ();
+		else
+			WheelBackToNormal ();
+
+		RotateShip ();
+		PanCamera ();
 	}
 
 	private void RotateWheel(){
 
-		//if turning left
-		if ((Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) && wheelRotation > -wheelRotationAmount) {
 
+		//if turning left
+		if ((Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) && wheelRotation > -wheelRotationAmount) {	
 			wheel.transform.Rotate (0, 0, wheelRotationSpeed * Time.deltaTime);
 			wheelRotation -= wheelRotationSpeed * Time.deltaTime;
 			shipRotation -= shipRotationSpeed * Time.deltaTime;
 
 			GetComponent<Animator> ().SetTrigger (turnLeftHash);
 			captain.GetComponent<Animator> ().SetTrigger (steerLeftHash);
-			PlayCaptainAnimation (1);
+			captain.GetComponent<Animator> ().speed = animationSpeed;
 
 			//if ship is at the rightmost point and returning to normal position, play animation at double speed.
 			if (shipRotation > 0) {
-				PlayCaptainAnimation (1);
+				captain.GetComponent<Animator> ().speed = animationSpeed * 2;
 			}
 
 		//if turning right
-		} else if ((Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) && wheelRotation < wheelRotationAmount) {
-			
+		} else if ((Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) && wheelRotation < wheelRotationAmount) {				
 			wheel.transform.Rotate (0, 0, -wheelRotationSpeed * Time.deltaTime);
 			wheelRotation += wheelRotationSpeed * Time.deltaTime;
 			shipRotation += shipRotationSpeed * Time.deltaTime;
 
 			GetComponent<Animator> ().SetTrigger (turnRightHash);
 			captain.GetComponent<Animator> ().SetTrigger (steerRightHash);
-			PlayCaptainAnimation (1);
+			captain.GetComponent<Animator> ().speed = animationSpeed;
 
 			//if ship is at the rightmost point and returning to normal position, play animation at double speed.
 			if (shipRotation < 0) {
-				PlayCaptainAnimation (2);
+				captain.GetComponent<Animator> ().speed = animationSpeed * 2;
 			}
 
 		//if keys are released, wheel return back to normal rotation
-		} else if (!Input.GetKey (KeyCode.A) &&
+		} else if ((!Input.GetKey (KeyCode.A) &&
 			!Input.GetKey (KeyCode.D) &&
 			!Input.GetKey (KeyCode.LeftArrow) &&
-			!Input.GetKey (KeyCode.RightArrow)) {
-
-			GetComponent<Animator> ().SetTrigger (idleHash);
-			PlayCaptainAnimation (1);
-
-			//when angle is close to zero, round it to zero
-			if (Mathf.Abs (shipRotation) < shipRotationSpeed * Time.deltaTime) {
-				shipRotation = 0;
-				captain.GetComponent<Animator> ().SetTrigger (idleCaptainHash);
-
-			}
-
-			//wheel return back to normal rotation
-
-			//if wheel was on the left
-			if (shipRotation < 0) {
-				wheelRotation += wheelRotationSpeed * Time.deltaTime;
-				shipRotation += shipRotationSpeed * Time.deltaTime;
-				wheel.transform.Rotate (0, 0, -wheelRotationSpeed * Time.deltaTime);
-				captain.GetComponent<Animator> ().SetTrigger (steerRightHash);
-				PlayCaptainAnimation (2);
-
-				//if wheel was on the right
-			} else if (shipRotation > 0) {
-				wheelRotation -= wheelRotationSpeed * Time.deltaTime;
-				shipRotation -= shipRotationSpeed * Time.deltaTime;
-				wheel.transform.Rotate (0, 0, wheelRotationSpeed * Time.deltaTime);
-				captain.GetComponent<Animator> ().SetTrigger (steerLeftHash);
-				PlayCaptainAnimation (2);
-
-			}
-
+			!Input.GetKey (KeyCode.RightArrow))) {
+			WheelBackToNormal ();
 		} else {
 			PauseAnimation ();
 		}
+	}
 
-		RotateShip ();
-		PanCamera ();
+	private void WheelBackToNormal(){
+		GetComponent<Animator> ().SetTrigger (idleHash);
+		captain.GetComponent<Animator> ().speed = animationSpeed;
+
+		//when angle is close to zero, round it to zero
+		if (Mathf.Abs (shipRotation) < shipRotationSpeed * Time.deltaTime) {
+			shipRotation = 0;
+			if (captainAtWheel) {
+				captain.GetComponent<Animator> ().SetTrigger (idleCaptainHash);
+			}
+		}
+
+		//wheel return back to normal rotation
+
+		//if wheel was on the left
+		if (shipRotation < 0) {
+			wheelRotation += wheelRotationSpeed * Time.deltaTime;
+			shipRotation += shipRotationSpeed * Time.deltaTime;
+			wheel.transform.Rotate (0, 0, -wheelRotationSpeed * Time.deltaTime);
+
+			if (captainAtWheel) {
+				captain.GetComponent<Animator> ().SetTrigger (steerRightHash);
+				//if ship is at the rightmost point and returning to normal position, play animation at double speed.
+				captain.GetComponent<Animator> ().speed = animationSpeed * 2;
+			}
+
+			//if wheel was on the right
+		} else if (shipRotation > 0) {
+			wheelRotation -= wheelRotationSpeed * Time.deltaTime;
+			shipRotation -= shipRotationSpeed * Time.deltaTime;
+			wheel.transform.Rotate (0, 0, wheelRotationSpeed * Time.deltaTime);
+
+			if (captainAtWheel) {
+				captain.GetComponent<Animator> ().SetTrigger (steerLeftHash);
+				//if ship is at the rightmost point and returning to normal position, play animation at double speed.
+				captain.GetComponent<Animator> ().speed = animationSpeed * 2;
+			}
+		}
 	}
 
 	private void RotateShip(){
@@ -130,9 +149,5 @@ public class MovementTest : MonoBehaviour {
 
 		//make wheel rotation zero so that it aligns with the captain's hands.
 		wheel.transform.localRotation = Quaternion.Euler (Vector3.zero);
-	}
-
-	private void PlayCaptainAnimation(float speedMultiplier = 1){
-		captain.GetComponent<Animator> ().speed = animationSpeed * speedMultiplier;
 	}
 }
